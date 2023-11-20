015eae8390e953e2b14f20f0dc12f475cedecd81
Ahmed Wagih <a.ahmedwageh@gmail.com>
2023-11-20 21:24:19 +0200

    update composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '371a5551f2a240cba41f2beb2623756af55059db',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -380,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '371a5551f2a240cba41f2beb2623756af55059db',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
