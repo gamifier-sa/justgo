2d1e70b3b5365aee3f10fd513a3c94a432cec87c
Ahmed Wagih <a.ahmedwageh@gmail.com>
2023-11-26 01:39:33 +0200

    update  times

--- app/Http/Resources/GymTimeResource.php
+++ app/Http/Resources/GymTimeResource.php
@@ -21,12 +21,11 @@ class GymTimeResource extends JsonResource
         $transformedData = [];
 
         foreach ($days as $index => $day) {
-            $times = $this->get();
+            $times = $this;
             if ($times->isNotEmpty()) {
                 $transformedData[] = [
                     $day => $times->groupBy('shift')->map(function ($timeGroup) use($day, $index){
                         return [
-                            'index' => $index,
                             'shift' => $timeGroup->first()->shift,
                             'open_at' => $timeGroup->where('type','open')->first()->$day,
                             'close_at' => $timeGroup->where('type','closed')->first()->$day
