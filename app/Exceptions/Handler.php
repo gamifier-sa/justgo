0f196ad6c66157567922ef6acce6f142d013d9ed
Ahmed-Wagih <a.ahmedwageh@gmail.com>
2023-11-27 08:55:16 +0000

    Update Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +28,24 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+
+        // Customize this part based on your needs
+        $statusCode = method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : 500;
+        $errorCode = $exception->getCode() ?: $statusCode;
+        $errorMessage = $exception->getMessage() ?: 'Internal Server Error';
+
+        return response()->json([
+            'error' => [
+                'code' => $errorCode,
+                'message' => $errorMessage,
+            ],
+        ], $statusCode);
     }
 }
